ddd9ba2361d4b796dad945039c02c6fdfa06ad12
Ahmad Saylam <saylamah@gmail.com>
2026-06-24 12:59:29 +0200

    Create Python package structure

---
+++ src/aop_framework/__init__.py
@@ -0,0 +1,8 @@
+"""
+AOP Kinetic Process Framework.
+
+Python tools for matrix-aware kinetic and process-level evaluation of
+advanced oxidation processes in wastewater treatment.
+"""
+
+__version__ = "0.1.0"
