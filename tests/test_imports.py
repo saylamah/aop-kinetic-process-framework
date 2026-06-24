383f55f0b05f063453d03541afa785c179aa77d3
Ahmad Saylam <saylamah@gmail.com>
2026-06-24 14:23:51 +0200

    Add package import tests

---
+++ tests/test_imports.py
@@ -0,0 +1,16 @@
+import aop_framework
+
+
+def test_package_version_exists():
+    assert aop_framework.__version__ == "0.1.0"
+
+
+def test_main_functions_are_exposed():
+    assert hasattr(aop_framework, "Species")
+    assert hasattr(aop_framework, "total_scavenging_capacity")
+    assert hasattr(aop_framework, "hydroxyl_radical_steady_state")
+    assert hasattr(aop_framework, "apparent_first_order_rate_constant")
+    assert hasattr(aop_framework, "hydroxyl_radical_production_from_ozone")
+    assert hasattr(aop_framework, "hydroxyl_radical_production_from_h2o2")
+    assert hasattr(aop_framework, "damkohler_number")
+    assert hasattr(aop_framework, "screen_aop_readiness")
