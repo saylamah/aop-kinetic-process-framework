e1fbfd53cd73d10f737434685165ba21c13de4ef
Ahmad Saylam <saylamah@gmail.com>
2026-06-24 14:22:40 +0200

    Expose main package functions

--- src/aop_framework/__init__.py
+++ src/aop_framework/__init__.py
@@ -5,4 +5,71 @@ Python tools for matrix-aware kinetic and process-level evaluation of
 advanced oxidation processes in wastewater treatment.
 """
 
+from aop_framework.energy import (
+    energy_per_mass_removed,
+    inverse_kapp_indicator,
+    volumetric_energy_consumption,
+)
+from aop_framework.h2o2 import (
+    hydroxyl_radical_generation_rate_from_h2o2_rate,
+    hydroxyl_radical_production_from_h2o2,
+)
+from aop_framework.kinetics import (
+    apparent_first_order_rate_constant,
+    first_order_remaining_fraction,
+    first_order_removal_fraction,
+    hydroxyl_radical_steady_state,
+    treatment_time_required,
+)
+from aop_framework.ozonation import (
+    hydroxyl_radical_generation_rate_from_ozone_rate,
+    hydroxyl_radical_production_from_ozone,
+)
+from aop_framework.reactor import (
+    cavitation_number,
+    damkohler_number,
+    optical_thickness,
+    reynolds_number,
+    sherwood_number,
+)
+from aop_framework.scavenging import (
+    Species,
+    radical_utilization_efficiency,
+    scavenging_table,
+    total_scavenging_capacity,
+)
+from aop_framework.treatment_train import (
+    AOPScreeningResult,
+    classify_scavenging_capacity,
+    screen_aop_readiness,
+)
+
 __version__ = "0.1.0"
+
+__all__ = [
+    "__version__",
+    "Species",
+    "AOPScreeningResult",
+    "total_scavenging_capacity",
+    "radical_utilization_efficiency",
+    "scavenging_table",
+    "hydroxyl_radical_steady_state",
+    "apparent_first_order_rate_constant",
+    "first_order_remaining_fraction",
+    "first_order_removal_fraction",
+    "treatment_time_required",
+    "volumetric_energy_consumption",
+    "energy_per_mass_removed",
+    "inverse_kapp_indicator",
+    "hydroxyl_radical_production_from_ozone",
+    "hydroxyl_radical_generation_rate_from_ozone_rate",
+    "hydroxyl_radical_production_from_h2o2",
+    "hydroxyl_radical_generation_rate_from_h2o2_rate",
+    "damkohler_number",
+    "reynolds_number",
+    "sherwood_number",
+    "optical_thickness",
+    "cavitation_number",
+    "classify_scavenging_capacity",
+    "screen_aop_readiness",
+]
