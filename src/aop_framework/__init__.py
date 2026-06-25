8e2f65820a475df37e9b2e503358bed887f19536
Ahmad Saylam <saylamah@gmail.com>
2026-06-25 08:15:51 +0200

    Fix package init formatting

--- src/aop_framework/__init__.py
+++ src/aop_framework/__init__.py
@@ -1,9 +1,10 @@
 """
 AOP Kinetic Process Framework.
 
-Python tools for matrix-aware kinetic and process-level evaluation of
-advanced oxidation processes in wastewater treatment.
+A lightweight scientific Python package for screening-level kinetic and
+process analysis of advanced oxidation processes in wastewater treatment.
 """
+
 from aop_framework.carbonate import (
     CarbonateSystemResult,
     carbonate_bicarbonate_ratio,
@@ -59,38 +60,37 @@ from aop_framework.treatment_train import (
 __version__ = "0.1.3"
 
 __all__ = [
-    "__version__",
-    "Species",
     "AOPScreeningResult",
     "CarbonateSystemResult",
-    "carbonate_bicarbonate_ratio",
-    "estimate_bicarbonate_carbonate_from_alkalinity",
-     "mg_L_to_mol_L",
-    "ug_L_to_mol_L",
-    "mol_L_to_mg_L",
-    "mol_L_to_ug_L",
-    "mg_C_L_to_mol_C_L",
+    "Species",
     "alkalinity_mg_CaCO3_L_to_eq_L",
-    "total_scavenging_capacity",
-    "radical_utilization_efficiency",
-    "scavenging_table",
-    "hydroxyl_radical_steady_state",
     "apparent_first_order_rate_constant",
-    "first_order_remaining_fraction",
-    "first_order_removal_fraction",
-    "treatment_time_required",
-    "volumetric_energy_consumption",
-    "energy_per_mass_removed",
-    "inverse_kapp_indicator",
-    "hydroxyl_radical_production_from_ozone",
-    "hydroxyl_radical_generation_rate_from_ozone_rate",
-    "hydroxyl_radical_production_from_h2o2",
-    "hydroxyl_radical_generation_rate_from_h2o2_rate",
-    "damkohler_number",
-    "reynolds_number",
-    "sherwood_number",
-    "optical_thickness",
+    "carbonate_bicarbonate_ratio",
     "cavitation_number",
     "classify_scavenging_capacity",
+    "damkohler_number",
+    "energy_per_mass_removed",
+    "estimate_bicarbonate_carbonate_from_alkalinity",
+    "first_order_remaining_fraction",
+    "first_order_removal_fraction",
+    "hydroxyl_radical_generation_rate_from_h2o2_rate",
+    "hydroxyl_radical_generation_rate_from_ozone_rate",
+    "hydroxyl_radical_production_from_h2o2",
+    "hydroxyl_radical_production_from_ozone",
+    "hydroxyl_radical_steady_state",
+    "inverse_kapp_indicator",
+    "mg_C_L_to_mol_C_L",
+    "mg_L_to_mol_L",
+    "mol_L_to_mg_L",
+    "mol_L_to_ug_L",
+    "optical_thickness",
+    "radical_utilization_efficiency",
+    "reynolds_number",
+    "scavenging_table",
     "screen_aop_readiness",
+    "sherwood_number",
+    "total_scavenging_capacity",
+    "treatment_time_required",
+    "ug_L_to_mol_L",
+    "volumetric_energy_consumption",
 ]
